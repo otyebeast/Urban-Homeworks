a829bfec3e1427eea59e68a8903dab4fa7cb7994
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-08-16 11:29:06 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Dmitry'
+print('Name:', name)
+age = 24
+print('Age:', age)
+age = age + 1
+print('New age:', age)
+is_student = True
+print('Is Student:', is_student)
---
+++ module_1_4.py
@@ -0,0 +1,17 @@
+# name = input("Введите Ваше имя: ")
+# current_year = 2024
+# date_of_birth = int(input("В каком году Вы родились? "))
+# age = current_year - date_of_birth
+# print("Здравствуйте,", name)
+# print("В этом году Вам",age, "лет")
+# print('привет, я строка в нижнем регистре'.replace('привет', "пока"))  # // .upper() .lower() .replace()
+
+# Task
+
+my_string = input('Your nickname: ')
+print(f'Количество символов в тексте: {len(my_string)}')
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
