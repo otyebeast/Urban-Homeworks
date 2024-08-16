8d0fdf2674170c054552be065e831496f4dce234
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-08-16 12:12:40 +0300

    Add files via upload

---
+++ Module 1/main.py
@@ -0,0 +1,36 @@
+print("Hello world")
+
+# 1st programm
+print("Задача 1. Ответ: " + str(9**0.5*5))
+
+# 2nd programm
+print(f"Задача 2. Ответ: {(9.99 > 9.98 and 1000 != 1000.1)}")
+
+# 3rd programm
+print(2*2+2)
+print(2*(2+2))
+print("Задача 3. Ответ: " + str((2*2+2) == (2*(2+2))))
+print('Задача 3. Ответ: False')
+
+# 4th programm
+y = float('123.456')
+print('Задача 4. Ответ:', int(y*10)%10)
+
+_str = '123.456'        # срез
+print(int(_str[4]))
+
+
+name = "Dimon"
+print(name[0])
+print(name[0:3])
+print(name[0:3:2])     # war[start:stop:step]
+print(name[:3])
+print(name[2:])
+print(name[::-1])
+
+example = 'Топинамбур'
+print(example[len(example)//2::2])
+print()
+# war[start:stop:step]
+
+
---
+++ Module 1/training.py
@@ -0,0 +1,9 @@
+example = 'Топинамбур'
+print(example[0])
+print(example[-1])
+print(example[len(example)//2:])     # // war[start:stop:step]
+print(example[::-1])
+print(example[1::2])
+
+
+
