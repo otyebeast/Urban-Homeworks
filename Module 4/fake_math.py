1be5c861ac5014c2f0efc5b71f8de4d16c59449d
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-09-09 11:34:44 +0300

    Add files via upload

---
+++ Module 4/fake_math.py
@@ -0,0 +1,7 @@
+def divide(first, second):
+    if second != 0:
+        otvet = first / second
+        return otvet
+    else:
+        otvet = 'Ошибка'
+        return otvet
---
+++ Module 4/true_math.py
@@ -0,0 +1,9 @@
+from math import inf
+
+
+def divide(first, second):
+    if second != 0:
+        otvet = first / second
+        return otvet
+    else:
+        return inf
