c94491955b88b4d4fb7e0ce55a47209218bf984c
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-09-17 10:29:39 +0300

    Add files via upload

---
+++ Module 5/module_5_2.py
@@ -0,0 +1,22 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название {self.name}, количество этажей: {self.number_of_floors}'
+
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
