1b107af0d56d1a91dcda23b0443b416078e59229
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-09-17 10:55:38 +0300

    Add files via upload

---
+++ Module 5/module_5_3.py
@@ -0,0 +1,79 @@
+class House:
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def go_to(self, new_floor):
+        for i in range(1, new_floor + 1):
+            if new_floor > self.number_of_floors or new_floor < 1:
+                print('Такого этажа не существует')
+                break
+            else:
+                print(i)
+
+    def __len__(self):
+        return self.number_of_floors
+
+    def __str__(self):
+        return f'Название {self.name}, количество этажей: {self.number_of_floors}'
+
+    def __eq__(self, other):
+        return self.number_of_floors == other.number_of_floors
+
+    def __lt__(self, other):
+        # (<)
+        return self.number_of_floors < other.number_of_floors
+
+    def __le__(self, other):
+        # (<=)
+        return self.number_of_floors <= other.number_of_floors
+
+    def __gt__(self, other):
+        # (>)
+        return self.number_of_floors > other.number_of_floors
+
+    def __ge__(self, other):
+        # (>=)
+        return self.number_of_floors >= other.number_of_floors
+
+    def __ne__(self, other):
+        # (!=)
+        return self.number_of_floors != other.number_of_floors
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+    def __radd__(self, value):
+        self.number_of_floors += value
+        return self
+
+    def __iadd__(self, value):
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+print(h1)
+print(h2)
+
+print(h1 == h2) # __eq__
+
+h1 = h1 + 10 # __add__
+print(h1)
+print(h1 == h2)
+
+h1 += 10 # __iadd__
+print(h1)
+
+h2 = 10 + h2 # __radd__
+print(h2)
+
+print(h1 > h2) # __gt__
+print(h1 >= h2) # __ge__
+print(h1 < h2) # __lt__
+print(h1 <= h2) # __le__
+print(h1 != h2) # __ne__
