d76940bd371917744424858b949edcd9c8907fdc
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-11-07 15:39:33 +0300

    Add files via upload

---
+++ Module 9/module_9_4.py
@@ -0,0 +1,35 @@
+from random import choice
+
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+result = list(map(lambda x, y: x == y, first, second))
+print(result)
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf8') as file:
+            for i in data_set:
+                file.write(str(i) + '\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+class MysticBall:
+    def __init__(self, *word):
+        self.words = word
+
+    def __call__(self):
+        word = choice(self.words)
+        return word
+
+
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
