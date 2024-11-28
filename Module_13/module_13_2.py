f51c40f9ad3565a430f5ffdee7951581b52a6015
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-11-28 17:57:24 +0300

    Add files via upload

---
+++ Module_13/module_13_2.py
@@ -0,0 +1,21 @@
+from aiogram import Bot, Dispatcher, types, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = ''
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print('Привет! Я бот, помогающий твоему здоровью.')
+
+
+@dp.message_handler()
+async def all_message(message):
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
