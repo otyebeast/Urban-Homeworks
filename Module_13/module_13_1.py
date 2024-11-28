785a370d5f176b887a5766d3d566d90adcc8a717
otyebeast <169045243+otyebeast@users.noreply.github.com>
2024-11-28 17:23:06 +0300

    Add files via upload

---
+++ Module_13/module_13_1.py
@@ -0,0 +1,19 @@
+import time
+import asyncio
+
+
+async def start_strongman(name, power):
+    print(f'Силач {name} начал соревнования.')
+    for i in range(5):
+        await asyncio.sleep(1 / power)
+        print(f'Силач {name} поднял {i+1} шар')
+    print(f'Силач {name} закончил соревнования.')
+
+async def start_tournament():
+    task1 = asyncio.create_task(start_strongman('Pasha', 3))
+    task2 = asyncio.create_task(start_strongman('Denis', 4))
+    task3 = asyncio.create_task(start_strongman('Apollon', 5))
+    await task1, task2, task3
+
+if __name__ == '__main__':
+    asyncio.run(start_tournament())
